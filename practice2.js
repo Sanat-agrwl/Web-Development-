b5eb128d6bab7fd73c191709fd88301fddb79567
Sanat Agrawal <sanatagrwl@gmail.com>
2023-06-28 22:59:53 +0530

    pct2

---
+++ practice2.js
@@ -0,0 +1,36 @@
+//1
+
+let age = 15;
+if (age > 10 && age < 20)
+    console.log("true");
+
+//2
+let b = 2;
+switch (b) {
+
+    case 1:
+        console.log("case1");
+        break;
+    case 2:
+        console.log("case2");
+        break;
+    case 3:
+        console.log("case3");
+        break;
+
+}
+//3
+let a = 12;
+if (a % 2 == 0 && a % 3 == 0)
+    console.log("true");
+
+//4
+
+let c = 12;
+if (c % 2 == 0 || c % 3 == 0)
+    console.log("true");
+
+
+//5
+s = age > 18 ? "adult" : "not an adult";
+console.log(s);
