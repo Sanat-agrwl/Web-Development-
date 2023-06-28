89f5d45a2d6ee389ae23e68c9933b87b82c186d7
Sanat Agrawal <sanatagrwl@gmail.com>
2023-06-28 22:07:37 +0530

    pct1

---
+++ practice1.js
@@ -0,0 +1,27 @@
+//1
+let a = "sanat";
+let b = a + 5;
+console.log(b);
+
+//2
+console.log(typeof b);
+
+//3
+const id = {
+    name: "sanat",
+    age: 19
+}
+//id = 19;  cannot do it
+
+//4
+id['student'] = true; //we can add keys to object
+console.log(id);
+
+//5
+const fruit = {
+    fruit1: "apple",
+    fruit2: "banana",
+    fruit3: "guava",
+
+}
+console.log(fruit);
--- variables.js
+++ variables.js
@@ -1,6 +1,30 @@
 // var , let ,const 
 //try to use const and let
-let a = 5;
-let b = "sanat";
-console.log(a);
-console.log(b);
+let num = 5;
+let name = "sanat";
+console.log(num);
+console.log(name);
+
+// NN SS BBU  primitive datatype in java
+// null number  symbol string   boolean BigInt  undefined  
+let a = null;
+let b = 56;
+
+let c = "sanat";
+let d = Symbol("i am symbol");
+
+let e = true;
+let f = BigInt(56);
+let g = undefined;
+console.log(a, b, c, d, e, f, g);
+
+//objects 
+const id = {
+
+    name: "sanat",
+    age: 19,
+    student: true
+}
+
+console.log(id["age"]);
+console.log(id.student);
