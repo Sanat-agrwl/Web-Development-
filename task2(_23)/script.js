29bc7669202f9309d942d3fc69e9d561298807c1
Sanat Agrawal <sanatagrwl@gmail.com>
2023-07-01 22:09:26 +0530

    alert,prompt

---
+++ task2(_23)/script.js
@@ -0,0 +1,20 @@
+// console functions 
+
+// console.log(console) // to view all funcs
+
+// console.clear()
+
+// console.table(obj) // to view object in a tabular format
+
+//to check time required for a process
+// console.time("a")
+// console.timeEnd("a")
+
+
+//alert("hii")
+
+
+//let a =prompt("enter a number","default value")
+//document.write(a)
+
+//  let b=confirm("are you sure")  // gives a true/false value
